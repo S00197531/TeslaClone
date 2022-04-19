3aec231320ccc2438c5100cb7888ca3803aaa2b0
Adam <adamsaul314@gmail.com>
2022-04-19 17:28:15 +0100

    done

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View} from 'react-native';
+import CarItem from './components/carItem';
+import CarList from './components/carsList';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <CarList />
+
     </View>
   );
 }
